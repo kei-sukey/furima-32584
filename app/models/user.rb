0180d201401588dbcdbf1df49d4956796c9bd5c6
kei-sukey <rough1diamond99k@gmail.com>
2020-12-16 16:30:41 +0900

    コードレビュー修正#1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,18 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
 
-  validates :nickname,        presence: true
   validates :password,        format: { with: PASSWORD_REGEX,
                                         message: 'Include both letters and numbers' }
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/,
-                                                        message: 'Full-width characters' }
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/,
-                                                        message: 'Full-width characters' }
-  validates :last_kana_name,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
-                                                        message: 'Full-width katakana characters' }
-  validates :first_kana_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/,
-                                                        message: 'Full-width katakana characters' }
-  validates :birthday,        presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/,
+                                    message: 'Full-width characters' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/,
+                                     message: 'Full-width characters' }
+    validates :last_kana_name, format: { with: /\A[ァ-ヶー－]+\z/,
+                                         message: 'Full-width katakana characters' }
+    validates :first_kana_name, format: { with: /\A[ァ-ヶー－]+\z/,
+                                          message: 'Full-width katakana characters' }
+    validates :birthday
+  end
 end
