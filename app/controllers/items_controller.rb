ce9f4b0155b56ef16f2ed2e0e8b12c83e831590c
kei-sukey <rough1diamond99k@gmail.com>
2020-12-21 11:36:12 +0900

    itemsコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,6 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all.order("created_at DESC")
-    # @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
