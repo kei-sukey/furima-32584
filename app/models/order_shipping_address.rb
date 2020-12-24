401b9ff3dfd3aa0c5ad346c803754482fc4224c9
kei-sukey <rough1diamond99k@gmail.com>
2020-12-24 11:07:43 +0900

    Formオブジェクトを用いた購入情報の登録機能

---
+++ app/models/order_shipping_address.rb
@@ -0,0 +1,21 @@
+class OrderShippingAddress
+
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :telephone_number, :item_id, :user_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/,
+                                     message: 'Input correctly' }
+    validates :prefecture_id, numericality: { other_than: 1,
+                                              message: 'Select' } 
+    validates :city
+    validates :block
+    validates :telephone_number, format: {with: /\A\d{10,11}\z/,
+                                          message: 'Input only number' }
+  end
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building, telephone_number: telephone_number, order_id: order.id)
+  end
+end
