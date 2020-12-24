287e9eb56887beb74a3daffc3798d90479ee1201
kei-sukey <rough1diamond99k@gmail.com>
2020-12-24 16:51:37 +0900

    PAY.JPを用いたクレジットカードでの決済機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,6 +12,7 @@ class OrdersController < ApplicationController
   def create
     @order_shipping_address = OrderShippingAddress.new(order_params)
     if @order_shipping_address.valid?
+      pay_item
       @order_shipping_address.save
       redirect_to root_path
     else
@@ -22,10 +23,19 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :telephone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :telephone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
